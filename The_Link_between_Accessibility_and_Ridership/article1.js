03eca8a2366e363c90b4002518a7dccdcacee774
rene78 <36115705+rene78@users.noreply.github.com>
2023-08-05 13:51:54 +0200

    reachPolygons & recommendations only on select

--- The_Link_between_Accessibility_and_Ridership/article1.js
+++ The_Link_between_Accessibility_and_Ridership/article1.js
@@ -1,43 +1,44 @@
-//All station names and numbers
+//All station names and numbers & empty object 'features' for Leaflet layer groups.
+//'Features' will be populated with reach polygons and recommendations for each station number later.
 const stations = {
-  1: 'PY01 Kwasa Damansara',
-  3: 'PY03 Kampung Selamat',
-  4: 'PY04 Sungai Buloh',
-  5: 'PY05 Damansara Damai',
-  6: 'PY06 Sri Damansara Barat',
-  7: 'PY07 Sri Damansara Sentral',
-  8: 'PY08 Sri Damansara Timur',
-  9: 'PY09 Metro Prima',
-  10: 'PY10 Kepong Baru',
-  11: 'PY11 Jinjang',
-  12: 'PY12 Sri Delima',
-  13: 'PY13 Kampung Batu',
-  14: 'PY14 Kentonmen',
-  15: 'PY15 Jalan Ipoh',
-  16: 'PY16 Sentul Barat',
-  17: 'PY17 Titiwangsa',
-  18: 'PY18 Hospital Kuala Lumpur',
-  19: 'PY19 Raja Uda',
-  20: 'PY20 Ampang Park',
-  21: 'PY21 Persiaran KLCC',
-  22: 'PY22 Conlay',
-  23: 'PY23 Tun Razak Exchange',
-  24: 'PY24 Chan Sow Lin',
-  25: 'PY25 Bandar Malaysia Utara',
-  26: 'PY26 Bandar Malaysia Selatan',
-  27: 'PY27 Kuchai',
-  28: 'PY28 Taman Naga Emas',
-  29: 'PY29 Sungai Besi',
-  31: 'PY31 Serdang Raya Utara',
-  32: 'PY32 Serdang Raya Selatan',
-  33: 'PY33 Serdang Jaya',
-  34: 'PY34 Universiti Putra Malaysia (UPM)',
-  36: 'PY36 Taman Equine',
-  37: 'PY37 Putra Permai',
-  38: 'PY38 16 Sierra',
-  39: 'PY39 Cyberjaya Utara',
-  40: 'PY40 Cyberjaya City Centre',
-  41: 'PY41 Putrajaya Sentral'
+  1: { name: 'PY01 Kwasa Damansara', features: undefined },
+  3: { name: 'PY03 Kampung Selamat', features: undefined },
+  4: { name: 'PY04 Sungai Buloh', features: undefined },
+  5: { name: 'PY05 Damansara Damai', features: undefined },
+  6: { name: 'PY06 Sri Damansara Barat', features: undefined },
+  7: { name: 'PY07 Sri Damansara Sentral', features: undefined },
+  8: { name: 'PY08 Sri Damansara Timur', features: undefined },
+  9: { name: 'PY09 Metro Prima', features: undefined },
+  10: { name: 'PY10 Kepong Baru', features: undefined },
+  11: { name: 'PY11 Jinjang', features: undefined },
+  12: { name: 'PY12 Sri Delima', features: undefined },
+  13: { name: 'PY13 Kampung Batu', features: undefined },
+  14: { name: 'PY14 Kentonmen', features: undefined },
+  15: { name: 'PY15 Jalan Ipoh', features: undefined },
+  16: { name: 'PY16 Sentul Barat', features: undefined },
+  17: { name: 'PY17 Titiwangsa', features: undefined },
+  18: { name: 'PY18 Hospital Kuala Lumpur', features: undefined },
+  19: { name: 'PY19 Raja Uda', features: undefined },
+  20: { name: 'PY20 Ampang Park', features: undefined },
+  21: { name: 'PY21 Persiaran KLCC', features: undefined },
+  22: { name: 'PY22 Conlay', features: undefined },
+  23: { name: 'PY23 Tun Razak Exchange', features: undefined },
+  24: { name: 'PY24 Chan Sow Lin', features: undefined },
+  25: { name: 'PY25 Bandar Malaysia Utara', features: undefined },
+  26: { name: 'PY26 Bandar Malaysia Selatan', features: undefined },
+  27: { name: 'PY27 Kuchai', features: undefined },
+  28: { name: 'PY28 Taman Naga Emas', features: undefined },
+  29: { name: 'PY29 Sungai Besi', features: undefined },
+  31: { name: 'PY31 Serdang Raya Utara', features: undefined },
+  32: { name: 'PY32 Serdang Raya Selatan', features: undefined },
+  33: { name: 'PY33 Serdang Jaya', features: undefined },
+  34: { name: 'PY34 Universiti Putra Malaysia (UPM)', features: undefined },
+  36: { name: 'PY36 Taman Equine', features: undefined },
+  37: { name: 'PY37 Putra Permai', features: undefined },
+  38: { name: 'PY38 16 Sierra', features: undefined },
+  39: { name: 'PY39 Cyberjaya Utara', features: undefined },
+  40: { name: 'PY40 Cyberjaya City Centre', features: undefined },
+  41: { name: 'PY41 Putrajaya Sentral', features: undefined }
 };
 
 //Leaflet map
@@ -52,7 +53,7 @@ function populateSelect() {
   let optionsHtml = '<option hidden disabled selected> Select a station </option>';
 
   for (const stationNumber in stations) {
-    const stationName = stations[stationNumber];
+    const stationName = stations[stationNumber].name;
     optionsHtml += `<option value="${stationNumber}">${stationName}</option>`;
   }
   document.querySelector("#stations").innerHTML = optionsHtml;
@@ -60,15 +61,24 @@ function populateSelect() {
 
 //Load the Putrajaya Line GeoJSON. Once this is done instantiate the Leaflet map and load the GeoJSON into the map
 async function loadGeoJsonAndDisplayMap() {
-  const response = await fetch("./data/putrajayaLineRecommendations.geojson");
+  const response = await fetch("./data/putrajayaLineRecommendations_alles.geojson");
   const geoJson = await response.json();
   //Add the base map without any layers.
   addLeafletMap();
+  //Create empty layer groups in 'stations' object
+  createEmptyLayerGroups();
   //Load the GeoJSON into the map
   showGeoJson(geoJson);
+  //Hide all reachPolygons and recommendations. Only show them when station is selected
+  hideReachPolygonsAndRecommendations();
 }
-// showGeoJson(putrajayaLineComments);
-// showGeoJson(putrajayaLine);
+
+//Create empty layer groups in 'stations' object
+function createEmptyLayerGroups() {
+  for (const stationNumber in stations) {
+    stations[stationNumber].features = L.layerGroup().addTo(map);
+  }
+}
 
 //Add a Leaflet map to the page
 function addLeafletMap() {
@@ -109,8 +119,14 @@ function showGeoJson(inputGeoJson) {
 
   //Define what happens when hovering over each polygon/way/marker
   function onEachFeature(feature, layer) {
-    // console.log(feature);
+    console.log(feature);
     // console.log(layer);
+
+    //Add reach polygons and recommendations to respective layer group in 'stations'
+    if (feature.properties.type === "recommendation" || feature.properties.type === "reachPolygons") {
+      console.log('called!');
+      stations[feature.properties["part-of"]].features.addLayer(layer);
+    }
 
     //Increase line weight on focus for recommendation features
     if (feature.properties.type === "recommendation") {
@@ -133,20 +149,8 @@ function showGeoJson(inputGeoJson) {
       /*
       console.log(e);
       console.log(e.target.feature.properties.title);
-      const mapContainer = document.querySelector("#map");
- 
-      //Popup with station infos
-      L.popup({
-        maxWidth: mapContainer.clientWidth - 45,
-        maxHeight: mapContainer.clientHeight - 40,
-        className: "stylePopup"
-      })
-        .setLatLng(e.latlng)
-        .setContent(popupHtml)
-        .openOn(map);
-        */
-    }
-    );
+      */
+    });
   };
 
   //Style the features
@@ -187,13 +191,13 @@ function generatePopupHtml(element) {
   }
   else if (type === "recommendation") {
     popupHtml = `
-      <h3 class="popup-heading">${stations[parseInt(element.feature.properties["part-of"])]}</h3>
+      <h3 class="popup-heading">${stations[parseInt(element.feature.properties["part-of"])].name}</h3>
       <div>${element.feature.properties.comment}</div>
       `;
   }
   else if (type === "reachPolygons") {
     popupHtml = `
-      <h3 class="popup-heading">${stations[parseInt(element.feature.properties["part-of"])]}</h3>
+      <h3 class="popup-heading">${stations[parseInt(element.feature.properties["part-of"])].name}</h3>
       <ul>
         <li>This area can be reached on foot within ${(element.feature.properties.label).trim()} from the station</li>
         <li>Population (estimated): ${element.feature.properties.total_pop}</li>
@@ -220,6 +224,13 @@ function generatePopupHtml(element) {
   })
     .setContent(popupHtml)
   return leafletPopup;
+}
+
+//Hide all reachPolygons and recommendations
+function hideReachPolygonsAndRecommendations() {
+  for (const stationNumber in stations) {
+    map.removeLayer(stations[stationNumber].features);
+  }
 }
 
 //If station has been selected in the dropdown list --> Zoom to associated map elements
@@ -249,3 +260,56 @@ stationsSelector.addEventListener('change', event => {
   map.fitBounds(elementsBelongingToSelectedStation.getBounds());
 
 });
+
+/*Proceed with:
+When a station is selected on drop down or on the map:
+- Remove all reachPolygons and recommendations
+- Display reachPolygons and recommendations for selected station
+- If it works --> Remove the 2 GeoJSONS and the temp below
+*/
+
+
+//Temp: Load GeoJSON recommendations of station 1
+let stationPutrajaLineAssessmentLayer;
+
+const wrapper = document.getElementById('wrapper');
+
+wrapper.addEventListener('click', async event => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+
+  let stationNumber = event.target.value;
+  console.log(stationNumber);
+  let stationGeoJsonToRender = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  //Remove old recommendations from map
+  if (stationPutrajaLineAssessmentLayer) map.removeLayer(stationPutrajaLineAssessmentLayer);
+
+  //Load GeoJSON with recommendations for Putrajaya Line
+  const response = await fetch("./data/putrajayaLineRecommendations.geojson");
+  const putrajayaLineRecommendations = await response.json();
+  for (let i = 0; i < putrajayaLineRecommendations.features.length; i++) {
+    if (putrajayaLineRecommendations.features[i].properties["part-of"] == stationNumber) {
+      stationGeoJsonToRender.features.push(putrajayaLineRecommendations.features[i])
+    }
+  }
+  // console.log(stationGeoJsonToRender);
+
+  //Add filtered GeoJSON to map
+  stationPutrajaLineAssessmentLayer = L.geoJSON(stationGeoJsonToRender,
+    // {
+    //   onEachFeature: onEachFeature,
+    //   style: style,
+    //   pointToLayer(feature, latlng) {
+    //     return L.marker(latlng, { icon: trainIcon });
+    //   }
+    // }
+  )
+  //Add GeoJSON to map
+  stationPutrajaLineAssessmentLayer.addTo(map);
+})
