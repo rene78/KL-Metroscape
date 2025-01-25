35f7670ed52f1d4cd7042b0c194701a74bf72bdd
rene78 <36115705+rene78@users.noreply.github.com>
2025-01-25 12:46:44 +0800

    - Image replaced with embedded YouTube video - Social Media image updated

--- darkmodechecker.js
+++ darkmodechecker.js
@@ -3,6 +3,6 @@ if (isDark) setDark();
 
 //Change website to dark mode if dark mode is set in OS
 function setDark() {
-  console.log('Dark mode set in OS! Change color scheme');
+  // console.log('Dark mode set in OS! Change color scheme');
   document.body.classList.toggle("dark-mode");
 }
